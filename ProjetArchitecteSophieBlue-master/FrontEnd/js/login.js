b0a9a91f91a4977a17e3f780a8a6ad261e52df71
Adam Hammouche <ahammouche@hotmail.com>
2023-02-16 15:20:42 +0100

    feat - ajout de la page log in

---
+++ ProjetArchitecteSophieBlue-master/FrontEnd/js/login.js
@@ -0,0 +1,41 @@
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    
+    const email = document.querySelector("#email").value;
+    const password = document.querySelector("#password").value;
+    
+    const user = { email, password };
+
+    fetch("http://localhost:5678/api/users/login", {
+        method: "POST",
+        body: JSON.stringify(user),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    }).then(async (response) => {
+        try {
+            const data = await response.json();
+            if (response.ok) {
+                window.location.href = "/ProjetArchitecteSophieBlue-master/FrontEnd/user.html";
+            } else {
+                alert("Login failed");
+            }
+        } catch(e) {
+            console.log(e);
+        }
+    });
+});
+
+  
+  
+  
+  
+
+    
+ 
+ 
+
+
+     
